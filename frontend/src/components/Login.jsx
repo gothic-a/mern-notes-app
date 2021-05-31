52c861cba59f21d65b86d7ca98e7127b8903fc1e
Vladyslav <v.polianychka@gmail.com>
2021-05-31 20:12:42 +0300

    Frontend validation register & login forms

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,38 +1,103 @@
 import { useState } from 'react'
+import { Formik } from 'formik'
+import * as yup from 'yup'
 
 const Login = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+
+    const formHandlerSubmit = (values) => {
+        console.log(values)
+    }
+
+    const validationSchema = yup.object().shape({
+        email: yup.string().typeError('must be string').required('email is required').email('email must be a valid'),
+        password: yup.string().typeError('must be string').required('password is required').min(6).max(18)
+    })
 
     return (
-        <form className="form form-login">
+        <Formik
+            initialValues={{
+                email: '',
+                password: '',
+            }}
+            validateOnBlur
+        
+            onSubmit={
+                (values) => formHandlerSubmit(values)
+            }
+            validationSchema={validationSchema}
+        >
+            
+            {
+                ({values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty, setFieldTouched}) => (
+                    <form className="form form-login" onSubmit={handleSubmit}>
+                       
+                       
+                        <div className="form__field">
+                            <label className="form__field-label" htmlFor="email">email:</label>
+                            <input 
+                                className={
+                                    `form__field-input ${errors.email && touched.email && `form__field-input_invalid`}`
+                                } 
+                                type="text" 
+                                id="email"
+                                autoComplete="off"
+                                formNoValidate
 
-            <div className="form__field">
-                <label className="form__field-label" htmlFor="email">email:</label>
-                <input 
-                    className="form__field-input" 
-                    type="email" 
-                    id="email"
-                    autoComplete="off"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-            </div>
-            <div className="form__field">
-                <label className="form__field-label" htmlFor="password">password:</label>
-                <input 
-                    className="form__field-input" 
-                    type="password" 
-                    id="password"
-                    autoComplete="off"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                />
-            </div>
-            <button className="btn form__button" type="submit">
-                sign in
-            </button>
-        </form>
+                                name="email"
+                                value={values.email}
+                                onChange={(e) => {
+                                    handleChange(e)
+                                    setFieldTouched('email', false)
+                                }}
+                                onBlur={handleBlur}
+                            />
+                            {
+                                errors.email 
+                                && touched.email 
+                                && <span className="form__field-validation-error">{errors.email}</span>
+                            }
+                            
+                        </div>
+                        <div className="form__field">
+                            <label className="form__field-label" htmlFor="password">
+                                password:
+                            </label>
+                            
+                            <input 
+                                className={
+                                    `form__field-input ${ errors.password && touched.password && `form__field-input_invalid`}`
+                                }  
+                                type="password" 
+                                id="password"
+                                autoComplete="off"
+
+                                name="password"
+                                value={values.password}
+                                onChange={(e) => {
+                                    handleChange(e)
+                                    setFieldTouched('password', false)
+                                }}
+                                onBlur={handleBlur}
+                            />
+                            {
+                                errors.password 
+                                && touched.password 
+                                && <span className="form__field-validation-error">{errors.password}</span>
+                            }
+
+                        </div>
+                        <button 
+                            className="btn form__button" 
+                            type="submit"
+                        >
+                            sign in
+                        </button>
+                    </form> 
+                )
+            }
+
+        </Formik>
+        
     )
 }
 
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,64 +1,142 @@
-import { useState } from 'react'
+import { Formik } from 'formik'
+import * as yup from 'yup'
+import { string } from 'yup/lib/locale'
 
 const Register = () => {
-    const [email, setEmail] = useState('')
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
+    const formHandlerSubmit = (values) => {
+        console.log(values)
+    }
+
+    const validationSchema = yup.object().shape({
+        email: yup.string().typeError('must be a string').required('email is required').email('email must be a valid'), 
+        name: yup.string().typeError('must be a string').required('name is required').min(3).max(24),
+        password: yup.string().typeError('must be a string').required('password is required').min(6).max(18),
+        confirmPassword: yup.string().typeError('must be a string').oneOf([yup.ref('password')], 'passwords missmatch').required('confirm your password')
+    })
 
     return (
-        <>
-            <form className="form form-register">
+        <Formik
+            initialValues={{
+                email: '',
+                name: '',
+                password: '',
+                confirmPassword: '',
+            }}
+            validateOnBlur
+            onSubmit={(values) => formHandlerSubmit(values)}
 
-                <div className="form__field">
-                    <label className="form__field-label" htmlFor="email">email:</label>
-                    <input 
-                        className="form__field-input" 
-                        type="email"
-                        id="email"
-                        autoComplete="off"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="form__field">
-                    <label className="form__field-label" htmlFor="name">name:</label>
-                    <input 
-                        className="form__field-input" 
-                        type="text"
-                        id="name"
-                        autoComplete="off"
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                    />
-                </div>
-                <div className="form__field">
-                    <label className="form__field-label" htmlFor="password">password:</label>
-                    <input 
-                        className="form__field-input" 
-                        type="password" 
-                        id="password"
-                        autoComplete="off"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className="form__field">
-                    <label className="form__field-label" htmlFor="confirmPassword">confirm password:</label>
-                    <input 
-                        className="form__field-input" 
-                        type="password" 
-                        id="confirmPassword"
-                        autoComplete="off"
-                        value={confirmPassword}
-                        onChange={e => setConfirmPassword(e.target.value)}
-                    />
-                </div>
-                <button className="btn form__button" type="submit">
-                    join
-                </button>
-            </form>
-        </>
+            validationSchema={validationSchema}
+        >
+            {
+                ({values, errors, touched, setFieldTouched, handleChange, handleSubmit, handleBlur}) => (
+
+                    <form className="form form-register" onSubmit={handleSubmit}>
+                        <div className="form__field">
+                            <label className="form__field-label" htmlFor="email">email:</label>
+                            <input 
+                                className={
+                                    `form__field-input ${ errors.email && touched.email && 'form__field-input_invalid' }`
+                                } 
+                                type="text"
+                                id="email"
+                                autoComplete="off"
+
+                                name="email"
+                                value={values.email}
+                                onChange={(e) => {
+                                    handleChange(e)
+                                    setFieldTouched('email', false)
+                                }}
+                                onBlur={handleBlur}
+                            />
+                            {
+                                errors.email 
+                                && touched.email 
+                                && <span className="form__field-validation-error">{errors.email}</span>
+                            }
+                        </div>
+                        <div className="form__field">
+                            <label className="form__field-label" htmlFor="name">name:</label>
+                            <input 
+                                className={
+                                    `form__field-input ${ errors.name && touched.name && 'form__field-input_invalid' }`
+                                } 
+                                type="text"
+                                id="name"
+                                autoComplete="off"
+
+                                name="name"
+                                value={values.name}
+                                onChange={(e) => {
+                                    handleChange(e)
+                                    setFieldTouched('name', false)
+                                }}
+                                onBlur={handleBlur}
+                            />
+                            {
+                                errors.name 
+                                && touched.name 
+                                && <span className="form__field-validation-error">{errors.name}</span>
+                            }
+                        </div>
+                        <div className="form__field">
+                            <label className="form__field-label" htmlFor="password">password:</label>
+                            <input 
+                                className={
+                                    `form__field-input ${errors.password && touched.password && 'form__field-input_invalid'}` 
+                                }
+                                type="password" 
+                                id="password"
+                                autoComplete="off"
+
+                                name="password"
+                                value={values.password}
+                                onChange={(e) => {
+                                    handleChange(e)
+                                    setFieldTouched('password', false)
+                                }}
+                                onBlur={handleBlur}
+                            />
+                            {
+                                errors.password 
+                                && touched.password 
+                                && <span className="form__field-validation-error">{errors.password}</span>
+                            }
+                        </div>
+                        <div className="form__field">
+                            <label className="form__field-label" htmlFor="confirmPassword">confirm password:</label>
+                            <input 
+                                className={
+                                    `form__field-input ${errors.confirmPassword && touched.confirmPassword && 'form__field-input_invalid'}` 
+                                }
+                                type="password" 
+                                id="confirmPassword"
+                                autoComplete="off"
+
+                                name="confirmPassword"
+                                value={values.confirmPassword}
+                                onChange={(e) => {
+                                    handleChange(e)
+                                    setFieldTouched('confirmPassword', false)
+                                }}
+                                onBlur={handleBlur}
+                            />
+                            {
+                                errors.confirmPassword 
+                                && touched.confirmPassword 
+                                && <span className="form__field-validation-error">{errors.confirmPassword}</span>
+                            }
+                        </div>
+                        <button 
+                            className="btn form__button" 
+                            type="submit"
+                        >
+                            join
+                        </button>
+                    </form>
+                )
+            }
+        </Formik>
     )
 }
 
